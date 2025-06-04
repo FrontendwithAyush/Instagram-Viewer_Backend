7384c61f0db60c29c3b673936938dafe9862a6d4
ayushkumbhalkar1 <ayushkumbhalkar68@gmail.com>
2025-06-04 11:49:19 +0530

    Fix Puppeteer Chromium issue for Render

--- saveCookies.js
+++ saveCookies.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    args: ["--no-sandbox"],
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
   const page = await browser.newPage();
